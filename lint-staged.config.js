dbae58dcec80eee858d3dfa7bcd40d750416a1c9
Mark <m4rc0d3r@gmail.com>
2025-01-23 16:11:49 +0200

    Use a basic lint-staged configuration for the entire project

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,31 +1,3 @@
-/**
- * @type {import('lint-staged').Configuration}
- */
-const config = {
-  "*.{json,md,yaml,yml,html,css}": [runPrettier()],
-  "*.{js,mjs,cjs,jsx}": [runEslint(), runPrettier()],
-};
+import base from "@tic-tac-toe/lint-staged-config/base";
 
-/**
- * @param {string} command
- * @param {string[]} [listOfFiles]
- */
-function executeCommand(command, listOfFiles) {
-  return listOfFiles ? `${command} ${listOfFiles}` : command;
-}
-
-/**
- * @param {string[]} [listOfFiles]
- */
-function runPrettier(listOfFiles) {
-  return executeCommand("prettier --write", listOfFiles);
-}
-
-/**
- * @param {string[]} [listOfFiles]
- */
-function runEslint(listOfFiles) {
-  return executeCommand("eslint --fix --flag unstable_config_lookup_from_file", listOfFiles);
-}
-
-export default config;
+export default base;
