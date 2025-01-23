1c1342501c763a189c7baaada8b330759f3164db
Mark <m4rc0d3r@gmail.com>
2025-01-24 00:40:07 +0200

    Add ES2015+ (ES6+) import/export syntax linting to ESLint base config

--- packages/eslint-config/src/base.js
+++ packages/eslint-config/src/base.js
@@ -1,5 +1,6 @@
 import pluginJs from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -12,5 +13,33 @@ export default [
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  {
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+    rules: {
+      "import/no-empty-named-blocks": "error",
+      "import/no-absolute-path": "error",
+      "import/no-self-import": "error",
+      "import/no-useless-path-segments": "error",
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "import/exports-last": "error",
+      "import/first": "error",
+      "import/group-exports": "error",
+      "import/newline-after-import": "error",
+      "import/no-named-default": "error",
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
   prettierConfig,
 ];
