936c65c81faa1a37ab832f0e0057fb1f4802693b
Mark <m4rc0d3r@gmail.com>
2025-01-25 22:21:33 +0200

    In the frontend application, configure Stylelint to work with Tailwind CSS

--- apps/frontend/stylelint.config.js
+++ apps/frontend/stylelint.config.js
@@ -1,2 +1,36 @@
 /** @type {import('stylelint').Config} */
-export default { extends: ["stylelint-config-standard", "stylelint-config-clean-order/error"] };
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-clean-order/error"],
+  rules: {
+    "at-rule-no-deprecated": [
+      true,
+      {
+        ignoreAtRules: ["apply"],
+      },
+    ],
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "theme",
+          "source",
+          "utility",
+          "variant",
+          "custom-variant",
+          "reference",
+          "config",
+          "plugin",
+        ],
+      },
+    ],
+    "declaration-property-value-no-unknown": [
+      true,
+      {
+        ignoreProperties: {
+          "/.+/": "/^(--spacing|theme)/",
+        },
+      },
+    ],
+    "import-notation": "string",
+  },
+};
