df17898d60c6b36a6d176c0eaf6ead656eb6f884
Mark <m4rc0d3r@gmail.com>
2025-02-13 04:37:35 +0200

    Configure lint-staged tasks related to package.json files

--- packages/lint-staged-config/src/base.js
+++ packages/lint-staged-config/src/base.js
@@ -1,10 +1,11 @@
-import { runEslint, runPrettier, runStylelint } from "./commands.js";
+import { runEslint, runPrettier, runStylelint, runSyncpackFormat } from "./commands.js";
 
 /** @type {import("lint-staged").Configuration} */
 const config = {
   "*.{json,md,yaml,yml,html}": [runPrettier()],
   "*.css": [runStylelint(), runPrettier()],
   "*.{js,mjs,cjs,jsx}": [runEslint(), runPrettier()],
+  "**/package.json": async (files) => [await runSyncpackFormat(files)],
 };
 
 export default config;
--- packages/lint-staged-config/src/commands.js
+++ packages/lint-staged-config/src/commands.js
@@ -1,7 +1,14 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import process from "node:process";
+
+const LINT_STAGED_CONFIG_FILE_NAMES = ["lint-staged.config.js"];
+
 const PRETTIER_COMMAND = "prettier --write";
 const STYLELINT_COMMAND = "stylelint --fix";
 const ESLINT_COMMAND = "eslint --fix --flag unstable_config_lookup_from_file";
 const TSC_COMMAND = "tsc --noEmit -p";
+const SYNCPACK_FORMAT_COMMAND = "syncpack format --source";
 
 /**
  * @param {string} command
@@ -31,4 +38,21 @@ function runTsc(pathToProject) {
   return executeCommand(TSC_COMMAND, pathToProject);
 }
 
-export { runEslint, runPrettier, runStylelint, runTsc };
+/** @param {string[]} listOfFiles */
+async function runSyncpackFormat(listOfFiles) {
+  const files = await Promise.all(
+    listOfFiles.map(async (file) => {
+      const relativePath = path.relative(process.cwd(), file);
+      const directoryContents = await fs.readdir(path.dirname(relativePath));
+      return LINT_STAGED_CONFIG_FILE_NAMES.some((fileName) => directoryContents.includes(fileName))
+        ? path.basename(relativePath)
+        : relativePath;
+    }),
+  );
+
+  const glob = files.length === 1 ? files.at(0) : `'{${files.join()}}'`;
+
+  return executeCommand(SYNCPACK_FORMAT_COMMAND, glob);
+}
+
+export { runEslint, runPrettier, runStylelint, runSyncpackFormat, runTsc };
