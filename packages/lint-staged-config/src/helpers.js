9c0f51a8c62c3c0af52c94d17d9663c0ccff8738
Mark <m4rc0d3r@gmail.com>
2025-02-16 00:59:18 +0200

    chore(lint-staged): add customization options when configuring tasks for TypeScript files
    Provide the ability to:
    - specify additional tasks;
    - change the order of task execution.

--- packages/lint-staged-config/src/helpers.js
+++ packages/lint-staged-config/src/helpers.js
@@ -4,17 +4,57 @@ import { runEslint, runPrettier, runTsc } from "./commands.js";
  * @typedef {object} Option
  * @property {string} glob
  * @property {string} pathToConfigFile
+ * @property {Record<string,string | (files: string[]) => string>} [additionalTasks]
+ * @property {SequenceOfExecution} [sequenceOfExecution]
  */
+
+/** @typedef {("prettier" | "eslint" | "tsc" | (string & Record<never, never>))[]} SequenceOfExecution */
 
 /** @param {Option[]} options */
 function setUpTasksForTypescriptFiles(options) {
-  return options.reduce((acc, { glob, pathToConfigFile }) => {
+  return options.reduce((acc, { glob, pathToConfigFile, additionalTasks, sequenceOfExecution }) => {
     acc[glob] = (files) => {
       const listOfFiles = files.join(" ");
-      return [runTsc(pathToConfigFile), runEslint(listOfFiles), runPrettier(listOfFiles)];
+
+      const normalizedDefaultTasks = Object.entries({
+        prettier: runPrettier(listOfFiles),
+        eslint: runEslint(listOfFiles),
+        tsc: runTsc(pathToConfigFile),
+      });
+
+      const normalizedAdditionalTasks = Object.entries(additionalTasks ?? {}).map(
+        ([name, command]) => [name, typeof command === "string" ? command : command(listOfFiles)],
+      );
+
+      const sequence = normalizeSequence(
+        normalizedAdditionalTasks.map(([name]) => name),
+        sequenceOfExecution,
+      );
+
+      const tasks = [...normalizedDefaultTasks, ...normalizedAdditionalTasks].sort(
+        ([aName], [bName]) => sequence.indexOf(aName) - sequence.indexOf(bName),
+      );
+
+      return tasks.map(([, command]) => command);
     };
     return acc;
   }, {});
+}
+
+const DEFAULT_EXECUTION_SEQUENCE = ["tsc", "eslint", "prettier"];
+
+/**
+ * @param {string[]} additionalTasks
+ * @param {SequenceOfExecution[]} [sequence]
+ */
+function normalizeSequence(additionalTasks, sequence) {
+  const normalized = new Set(sequence ?? DEFAULT_EXECUTION_SEQUENCE);
+
+  for (const task of [...additionalTasks, ...DEFAULT_EXECUTION_SEQUENCE]) {
+    normalized.add(task);
+  }
+
+  return [...normalized];
 }
 
 export { setUpTasksForTypescriptFiles };
