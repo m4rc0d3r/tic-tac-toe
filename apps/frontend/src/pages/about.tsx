d91f96ef8bd4bd2b97418686bd2f348079f4666d
Mark <m4rc0d3r@gmail.com>
2025-02-18 01:36:57 +0200

    feat(frontend): create a home page and an about page
    Create a few pages that will be needed as stubs for functionality created in future commits.

---
+++ apps/frontend/src/pages/about.tsx
@@ -0,0 +1,5 @@
+function AboutPage() {
+  return <p className="flex h-full items-center justify-center">Welcome to the About Us page.</p>;
+}
+
+export { AboutPage };
---
+++ apps/frontend/src/pages/home.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <p className="flex h-full items-center justify-center">Welcome to the home page.</p>;
+}
+
+export { HomePage };
