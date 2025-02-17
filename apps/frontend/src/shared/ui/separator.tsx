e0a20a6ae22b2663ef330726b9f27cfe85f99d37
Mark <m4rc0d3r@gmail.com>
2025-02-18 00:36:55 +0200

    feat(frontend): provide the ability to specify the thickness of the separator

--- apps/frontend/src/shared/ui/separator.tsx
+++ apps/frontend/src/shared/ui/separator.tsx
@@ -3,19 +3,29 @@ import * as React from "react";
 
 import { cn } from "./utils";
 
+type SeparatorProps = React.ComponentProps<typeof SeparatorPrimitive.Root> & {
+  thickness?: string | undefined;
+};
+
 function Separator({
   className,
   orientation = "horizontal",
   decorative = true,
+  thickness = "2px",
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: SeparatorProps) {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator-root"
       decorative={decorative}
       orientation={orientation}
+      style={
+        {
+          "--thickness": thickness,
+        } as React.CSSProperties
+      }
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        `bg-border shrink-0 data-[orientation=horizontal]:h-[var(--thickness)] data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-[var(--thickness)]`,
         className,
       )}
       {...props}
