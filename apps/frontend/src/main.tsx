4adc2be03d8f1e064676acbabb8096e2b36722fa
Mark <m4rc0d3r@gmail.com>
2025-02-18 01:02:52 +0200

    feat(frontend): connect sonner (toast) component to the application

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,10 +5,12 @@ import App from "./App.tsx";
 import { Configurator } from "./app/configurator.tsx";
 import { QueryClientProvider } from "./app/react-query.tsx";
 import "./index.css";
+import { Toaster } from "./shared/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Configurator />
+    <Toaster />
     <QueryClientProvider>
       <App />
     </QueryClientProvider>
