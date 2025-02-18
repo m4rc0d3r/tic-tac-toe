cf12b66af86919520c2c82c5e2e6bf2b9a7a5dde
Mark <m4rc0d3r@gmail.com>
2025-02-18 17:49:50 +0200

    feat(frontend): create type for tRPC error cause

--- apps/frontend/src/shared/api/trpc.ts
+++ apps/frontend/src/shared/api/trpc.ts
@@ -3,4 +3,7 @@ import { createTRPCReact } from "@trpc/react-query";
 
 const trpc: ReturnType<typeof createTRPCReact<AppRouter>> = createTRPCReact<AppRouter>();
 
+type TrpcErrorCause = ReturnType<AppRouter["_def"]["_config"]["errorFormatter"]>["cause"];
+
 export { trpc };
+export type { TrpcErrorCause };
