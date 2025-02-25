e2887346748f7b0dd6d79fdd958579e26eb94fbd
Mark <m4rc0d3r@gmail.com>
2025-02-25 04:22:43 +0200

    feat(frontend): create root layout
    The layout should consist of:
    - a header containing a navigation bar and some information about the current user if logged in;
    - a component related to the current route.

---
+++ apps/frontend/src/app/root-layout.tsx
@@ -0,0 +1,164 @@
+import { MenuIcon, UserIcon } from "lucide-react";
+import type { ComponentProps } from "react";
+import { Link, Outlet, useNavigate } from "react-router";
+import { toast } from "sonner";
+
+import type { Me } from "~/entities/auth";
+import { useAuthStore } from "~/entities/auth";
+import { trpc } from "~/shared/api";
+import AboutIcon from "~/shared/assets/about.svg?react";
+import LogoIcon from "~/shared/assets/logo.svg?react";
+import { ROUTES } from "~/shared/routing";
+import { Button } from "~/shared/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/shared/ui/dropdown-menu";
+import { cn } from "~/shared/ui/utils";
+
+function RootLayout() {
+  type State = ReturnType<(typeof useAuthStore)["getState"]>;
+  const { status, me } = {
+    status: useAuthStore.use.status(),
+    me: useAuthStore.use.me(),
+  } as
+    | Pick<Extract<State, { status: "UNCERTAIN" | "UNAUTHENTICATED" }>, "status" | "me">
+    | Pick<Extract<State, { status: "AUTHENTICATED" }>, "status" | "me">;
+
+  const menuItems = [
+    {
+      name: "About",
+      path: ROUTES.about,
+      Icon: AboutIcon,
+    },
+  ];
+
+  const authMenuItems = [
+    {
+      name: "Sign in",
+      path: ROUTES.login,
+      variant: "ghost" satisfies ComponentProps<typeof Button>["variant"],
+    },
+    {
+      name: "Sign up",
+      path: ROUTES.registration,
+      variant: "outline" satisfies ComponentProps<typeof Button>["variant"],
+    },
+  ] as const;
+
+  return (
+    <div className="flex h-full flex-col">
+      <header>
+        <nav>
+          <ul className="flex justify-between p-2 shadow-sm">
+            <li className="flex items-center md:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" size="icon">
+                    <MenuIcon />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  {menuItems.map(({ name, path, Icon }) => (
+                    <DropdownMenuItem key={path} asChild>
+                      <Link to={path}>
+                        <Icon />
+                        {name}
+                      </Link>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </li>
+            <li>
+              <Link to={ROUTES.home}>
+                <LogoIcon className="size-10" />
+              </Link>
+            </li>
+            <li className="hidden items-center md:flex">
+              <ul className="flex">
+                {menuItems.map(({ name, path }) => (
+                  <li key={path}>
+                    <Button asChild variant="link">
+                      <Link to={path}>{name}</Link>
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="flex items-center">
+              {status === "AUTHENTICATED" ? (
+                <MeSection data={me} />
+              ) : status === "UNAUTHENTICATED" ? (
+                <ul className="flex gap-1">
+                  {authMenuItems.map(({ name, path, variant }, index) => (
+                    <li
+                      key={path}
+                      className={cn(index === authMenuItems.length - 1 && "hidden sm:list-item")}
+                    >
+                      <Button asChild variant={variant}>
+                        <Link to={path}>{name}</Link>
+                      </Button>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <></>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="h-full">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+type MeSectionProps = ComponentProps<typeof DropdownMenu> & {
+  data: Me;
+};
+
+function MeSection({ data, ...props }: MeSectionProps) {
+  const navigate = useNavigate();
+
+  const { mutate: logout, isPending: isLogoutPending } = trpc.auth.logout.useMutation();
+  const logoutLocally = useAuthStore.use.logout();
+
+  const handleLogout = () => {
+    logout(void 0, {
+      onSuccess: () => {
+        toast.success("Logout completed successfully.");
+      },
+      onError: () => {
+        toast.error("Errors occurred while logging out.");
+      },
+      onSettled: () => {
+        logoutLocally();
+        void navigate(ROUTES.home);
+      },
+    });
+  };
+
+  const { firstName, lastName } = data;
+  const fullName = [firstName, lastName].join(" ").trim();
+
+  return (
+    <DropdownMenu {...props}>
+      <DropdownMenuTrigger className="hover:bg-muted flex cursor-pointer items-center rounded-md px-2">
+        <UserIcon className="size-10" />
+        {fullName || "Name not specified"}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={handleLogout} disabled={isLogoutPending}>
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export { RootLayout };
--- apps/frontend/src/app/router.ts
+++ apps/frontend/src/app/router.ts
@@ -1,5 +1,7 @@
 import { createBrowserRouter, Outlet } from "react-router";
 
+import { RootLayout } from "./root-layout";
+
 import { withConditionalAccess } from "~/features/routing2";
 import { AboutPage } from "~/pages/about";
 import { HomePage } from "~/pages/home";
@@ -12,12 +14,17 @@ const router = createBrowserRouter([
     path: "/",
     children: [
       {
-        index: true,
-        Component: HomePage,
-      },
-      {
-        path: ROUTES.about,
-        Component: AboutPage,
+        Component: RootLayout,
+        children: [
+          {
+            index: true,
+            Component: HomePage,
+          },
+          {
+            path: ROUTES.about,
+            Component: AboutPage,
+          },
+        ],
       },
       {
         Component: withConditionalAccess({
