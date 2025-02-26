058a8a14869d061157f79859fef14dd02bf4d240
Mark <m4rc0d3r@gmail.com>
2025-02-26 23:03:31 +0200

    feat(frontend): extend hook for i18n by adding post-processing of translations
    Provide the ability to:
    - capitalize the first letter of the translation;
    - format the translation as a sentence.

--- apps/frontend/src/shared/i18n.ts
+++ apps/frontend/src/shared/i18n.ts
@@ -1,3 +1,9 @@
+import { capitalize } from "@tic-tac-toe/core";
+import type { FlatNamespace, KeyPrefix, Namespace, TFunction } from "i18next";
+import type { $Tuple } from "node_modules/react-i18next/helpers";
+import type { FallbackNs, UseTranslationOptions, UseTranslationResponse } from "react-i18next";
+import { useTranslation } from "react-i18next";
+
 const RESOURCES = {
   en: {
     translation: {},
@@ -14,5 +20,55 @@ const SUPPORTED_LANGUAGES: Record<LanguageCode, string> = {
   uk: "ukrainian",
 };
 
-export { RESOURCES, SUPPORTED_LANGUAGES };
+type TFunctionWithPostprocessor<
+  Ns extends FlatNamespace | $Tuple<FlatNamespace> | undefined = undefined,
+  KPrefix extends KeyPrefix<FallbackNs<Ns>> = undefined,
+  Result extends string = string,
+> = (...args: Parameters<TFunction<FallbackNs<Ns>, KPrefix>>) => Result;
+
+function useTranslation2<
+  Ns extends FlatNamespace | $Tuple<FlatNamespace> | undefined = undefined,
+  KPrefix extends KeyPrefix<FallbackNs<Ns>> = undefined,
+>(
+  ns?: Ns,
+  options?: UseTranslationOptions<KPrefix>,
+): {
+  translation: UseTranslationResponse<FallbackNs<Ns>, KPrefix>;
+  postproc: {
+    tc: TFunctionWithPostprocessor<FallbackNs<Ns>, KPrefix, Capitalize<string>>;
+    ts: (punctuationMark: string) => TFunctionWithPostprocessor<FallbackNs<Ns>, KPrefix>;
+    tsp: TFunctionWithPostprocessor<FallbackNs<Ns>, KPrefix>;
+  };
+} {
+  const translation = useTranslation(ns, options);
+
+  const { t } = translation;
+
+  const postproc = {
+    tc: createTc(t),
+    ts: createTs(t),
+    tsp: createTsp(t),
+  };
+
+  return {
+    translation,
+    postproc,
+  };
+}
+
+function createTc<Ns extends Namespace, KPrefix>(t: TFunction<Ns, KPrefix>) {
+  return (...args: Parameters<typeof t>) => capitalize(t(...args));
+}
+
+function createTs<Ns extends Namespace, KPrefix>(t: TFunction<Ns, KPrefix>) {
+  return (punctuationMark: string) =>
+    (...args: Parameters<typeof t>) =>
+      [createTc(t)(...args), punctuationMark].join("");
+}
+
+function createTsp<Ns extends Namespace, KPrefix>(t: TFunction<Ns, KPrefix>) {
+  return (...args: Parameters<typeof t>) => createTs(t)(".")(...args);
+}
+
+export { createTc, createTs, createTsp, RESOURCES, SUPPORTED_LANGUAGES, useTranslation2 };
 export type { LanguageCode };
