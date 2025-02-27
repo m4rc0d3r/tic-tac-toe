c5d9c3faf5ae77f9f73da210f9a54e2747328526
Mark <m4rc0d3r@gmail.com>
2025-02-28 00:51:48 +0200

    fix: make the function that capitalizes the first letter do just that and nothing else
    This function should not convert all letters except the first one to lowercase.

--- packages/core/src/converters/string.ts
+++ packages/core/src/converters/string.ts
@@ -1,9 +1,7 @@
 import { EMPTY_STRING } from "~/constants";
 
 function capitalize<T extends string>(value: T): Capitalize<T> {
-  return [value.at(0)?.toLocaleUpperCase(), value.slice(1).toLocaleLowerCase()].join(
-    EMPTY_STRING,
-  ) as Capitalize<T>;
+  return [value.at(0)?.toLocaleUpperCase(), value.slice(1)].join(EMPTY_STRING) as Capitalize<T>;
 }
 
 export { capitalize };
