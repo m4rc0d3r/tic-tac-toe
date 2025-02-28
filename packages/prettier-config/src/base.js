79a45f379ce371aed306149e6fcdfba288c1375e
Mark <m4rc0d3r@gmail.com>
2025-02-28 22:04:17 +0200

    chore(prettier): extend the base configuration to support SVG file formatting

--- packages/prettier-config/src/base.js
+++ packages/prettier-config/src/base.js
@@ -2,6 +2,14 @@
 const config = {
   printWidth: 100,
   plugins: ["prettier-plugin-jsdoc"],
+  overrides: [
+    {
+      files: "*.svg",
+      options: {
+        plugins: ["@prettier/plugin-xml"],
+      },
+    },
+  ],
 };
 
 export default config;
