396052b82e9b615e6fea9aee3828d83e9f83b352
Mark <m4rc0d3r@gmail.com>
2025-05-17 01:03:22 +0300

    chore(prettier): configure to format SQL files

--- packages/prettier-config/src/base.js
+++ packages/prettier-config/src/base.js
@@ -1,7 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
   printWidth: 100,
-  plugins: ["prettier-plugin-jsdoc"],
+  plugins: ["prettier-plugin-jsdoc", "prettier-plugin-sql"],
+  language: "postgresql",
+  keywordCase: "upper",
+  dataTypeCase: "upper",
+  functionCase: "lower",
+  identifierCase: "lower",
   overrides: [
     {
       files: "*.svg",
