03b8ddeb67da8a1f8f65697541872b16b6d420ed
Mark <m4rc0d3r@gmail.com>
2025-05-17 05:36:52 +0300

    chore(prettier): configure to format SQL in JS tagged template literals

--- packages/prettier-config/src/base.js
+++ packages/prettier-config/src/base.js
@@ -1,12 +1,18 @@
 /** @type {import("prettier").Config} */
 const config = {
   printWidth: 100,
-  plugins: ["prettier-plugin-jsdoc", "prettier-plugin-sql"],
+  plugins: ["prettier-plugin-jsdoc", "prettier-plugin-sql", "prettier-plugin-embed"],
   language: "postgresql",
   keywordCase: "upper",
   dataTypeCase: "upper",
   functionCase: "lower",
   identifierCase: "lower",
+  embeddedSqlTags: [
+    "this.prisma.$queryRaw",
+    "this.prisma.$executeRaw",
+    "tx.$queryRaw",
+    "tx.$executeRaw",
+  ],
   overrides: [
     {
       files: "*.svg",
